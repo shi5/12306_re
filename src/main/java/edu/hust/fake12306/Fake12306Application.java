55f8ea8e6930212af1462f3cf3ef253be0a9f01f
shi5 <1874155789@qq.com>
2023-10-22 17:25:35 +0800

    Initial commit

---
+++ src/main/java/edu/hust/fake12306/Fake12306Application.java
@@ -0,0 +1,14 @@
+package edu.hust.fake12306;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Fake12306Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Fake12306Application.class, args);
+    }
+
+}
+
---
+++ src/test/java/edu/hust/fake12306/Fake12306ApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.hust.fake12306;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Fake12306ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
